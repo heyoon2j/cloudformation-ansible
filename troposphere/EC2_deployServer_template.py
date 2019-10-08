64a7f4100284acaf64bbfc000128820ecb577cd9
Yoon <jysz93@naver.com>
2019-10-08 11:03:38 +0000

    qwe

--- troposphere/EC2_deployServer_template.py
+++ troposphere/EC2_deployServer_template.py
@@ -8,6 +8,7 @@ from awacs.sts import AssumeRole
 
 AnsiblePlaybookFile = "ansible/deployServer.yml"
 ApplicationPort = "80"
+HostFilePath="inventory/hosts"
 GithubAnsibleURL = "https://github.com/yoon2ix/cloudformation-ansible.git"
 VpvID = "vpc-0a93272040286fd79"
 SubnetID = "subnet-07f69f1a00576f7de"
@@ -20,7 +21,7 @@ t= Template()
 t.add_description("Effective DevOps in AWS: Deploy Template")
 
 # Deploy Server for Ansible
-AnsiblePullCmd = "/usr/bin/ansible-pull -U {} {}".format(GithubAnsibleURL, AnsiblePlaybookFile)
+AnsiblePullCmd = "/usr/bin/ansible-pull -U {} {} -i".format(GithubAnsibleURL, AnsiblePlaybookFile, HostFilePath)
 
 
 # AWS IAM Profile
