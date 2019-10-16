a07e2aa2e130436c05e8fa72775b1717f128009b
Yoon <jysz93@naver.com>
2019-10-16 07:23:11 +0000

    add setWSGI.sh

---
+++ ansible/roles/codingTestServer/files/setWSGIModule.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+touch /etc/httpd/conf.modules.d/mod_wsgi.conf
+mod_wsgi-express module-config > /etc/httpd/conf.modules.d/mod_wsgi.conf
+
+
+documentRoot='/usr/local/django/codingTest'
+projectName='codingTest'
+envPath='/usr/local/env'
+
+echo "ServerName localhost" >> /etc/httpd/conf/httpd.conf
+sed -i "s/Listen 80/Listen 8000/g" /etc/httpd/conf/httpd.conf
+
+touch /etc/httpd/conf.d/vhost.conf
+cat << EOF > /etc/httpd/conf.d/vhost.conf
+<VirtualHost *:8000>
+
+	ServerName localhost
+	DocumentRoot $documentRoot
+
+	Alias /robots.txt $documentRoot/robots.txt
+	Alias /favicon.ico $documentRoot/favicon.ico
+
+	Alias /media/ $documentRoot/media/
+	Alias /static/ $documentRoot/static/
+
+	<Directory $documentRoot>
+		<IfVersion < 2.4>
+			Order allow,deny
+			Allow from all
+		</IfVersion>
+		<IfVersion >= 2.4>
+			Require all granted
+		</IfVersion>
+	</Directory>
+
+	<Directory $documentRoot/media>
+		<IfVersion < 2.4>
+			Order allow,deny
+			Allow from all
+		</IfVersion>
+		<IfVersion >= 2.4>
+			Require all granted
+		</IfVersion>
+	</Directory>
+
+	<Directory $documentRoot/static>
+		<IfVersion < 2.4>
+			Order allow,deny
+			Allow from all
+		</IfVersion>
+		<IfVersion >= 2.4>
+			Require all granted
+		</IfVersion>
+	</Directory>
+
+	<Directory $documentRoot/media>
+		<IfVersion < 2.4>
+			Order allow,deny
+			Allow from all
+		</IfVersion>
+		<IfVersion >= 2.4>
+			Require all granted
+		</IfVersion>
+	</Directory>
+
+	<Directory $documentRoot/static>
+		<IfVersion < 2.4>
+			Order allow,deny
+			Allow from all
+		</IfVersion>
+		<IfVersion >= 2.4>
+			Require all granted
+		</IfVersion>
+	</Directory>
+
+	WSGIDaemonProcess $projectName python-home=$envPath/$projectName python-path=$documentRoot
+	WSGIProcessGroup $projectName
+
+	WSGIScriptAlias / $documentRoot/$projectName/wsgi.py
+
+	<Directory $documentRoot/$projectName>
+		<IfVersion < 2.4>
+			Order allow,deny
+			Allow from all
+		</IfVersion>
+		<IfVersion >= 2.4>
+			Require all granted
+		</IfVersion>
+	</Directory>
+</VirtualHost>
+EOF
