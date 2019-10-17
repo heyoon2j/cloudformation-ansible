021ead338168555e78b129c04a9f017953d585ea
Yoon <jysz93@naver.com>
2019-10-17 10:23:44 +0000

    123

--- ansible/roles/codingTestServer/files/setProject.sh
+++ ansible/roles/codingTestServer/files/setProject.sh
@@ -10,7 +10,7 @@ echo "export PublicIP=\$(curl ipv4.icanhazip.com)" >> /root/.bashrc
 . /root/.bashrc
 
 sed -i "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/g" $documentRoot/$projectName/settings.py
-sed -i "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'codingTest', \n        'UESR': 'codingTest',\n        'PASSWORD': 'It12345!',\n        'HOST': 'codingtestdb.cq7rwyqbhtdi.ap-northeast-2.rds.amazonaws.com',\n        'PORT': '3306',\n/g" $documentRoot/$projectName/settings.py
+sed -i "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3')/'NAME': 'codingTest', \n        'UESR': 'codingTest',\n        'PASSWORD': 'It12345!',\n        'HOST': 'codingtestdb.cq7rwyqbhtdi.ap-northeast-2.rds.amazonaws.com',\n        'PORT': '3306',\n/g" $documentRoot/$projectName/settings.py
 
 sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['$PublicIP'\]/g" $documentRoot/$projectName/settings.py
 
--- ansible/roles/manageServer/files/setProject.sh
+++ ansible/roles/manageServer/files/setProject.sh
@@ -12,6 +12,11 @@ echo "export PublicIP=\$(curl ipv4.icanhazip.com)" >> /root/.bashrc
 sed -i "s/'ENGINE': 'django.db.backends.sqlite3',/'ENGINE': 'django.db.backends.mysql',/g" $documentRoot/$projectName/settings.py
 sed -i "s/'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),/'NAME': 'manageDB', \n        'UESR': 'manage',\n        'PASSWORD': 'It12345!',\n        'HOST': 'manage.cq7rwyqbhtdi.ap-northeast-2.rds.amazonaws.com',\n        'PORT': '3306',\n/g" $documentRoot/$projectName/settings.py
 
+
+sed -i "s/'NAME': os.path.join\(BASE_DIR, 'db.sqlite3'\),/'NAME': 'manageDB', \n        'UESR': 'manage',\n        'PASSWORD': 'It12345!',\n        'HOST': 'manage.cq7rwyqbhtdi.ap-northeast-2.rds.amazonaws.com',\n        'PORT': '3306',\n/g" $documentRoot/$projectName/settings.py
+
+
+
 sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['$PublicIP'\]/g" $documentRoot/$projectName/settings.py
 
 sed -i "s/INSTALLED_APPS = \[/INSTALLED_APPS = \[\n    '$appName',/g" $documentRoot/$projectName/settings.py
